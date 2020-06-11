d2172343ace722ebb240e1b43486f270f71ef93f
sdsshi <SDS192014@163.com>
2020-06-11 10:57:47 +0800

    Initial commit

---
+++ src/First/Helloworld.java
@@ -0,0 +1,17 @@
+package First;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello_world");
+        List<Integer> list = new ArrayList<>();
+
+
+
+
+
+    }
+}
